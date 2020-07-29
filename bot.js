252132b47f3736026e521f522a8b1e8ba598260b
zeyadYT <68921480+zeyadYT@users.noreply.github.com>
2020-07-29 03:23:57 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const myid = ['ID'];
+const myid = ['493425830610075648'];
 const prefix = ['$'];
 const client = new Discord.Client();
 const client2 = new Discord.Client();
